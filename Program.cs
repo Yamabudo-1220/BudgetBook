276e8e8bf0aa2ed34fc4ca33fcffa15cba5e5bd9
Yamabudo-1220 <sorinhudusan90@gmail.com>
2026-09-16 13:52:51 +0200

    Refactor HomeController, update routing to Transactions, and remove unused views

--- Program.cs
+++ Program.cs
@@ -45,7 +45,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}")
+	pattern: "{controller=Transactions}/{action=Index}/{id?}")
 	.WithStaticAssets();
 
 app.MapRazorPages()
